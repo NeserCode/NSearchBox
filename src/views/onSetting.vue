8704bbe780386711ecbfb03cecf60480fbd636d6
✨NeserCode <nesercode@gmail.com>
2022-10-06 19:45:21 +0800

    Update onSetting.vue

--- src/views/onSetting.vue
+++ src/views/onSetting.vue
@@ -8,6 +8,8 @@
 <style lang="postcss" scoped>
 .setting-main {
   @apply w-full p-2 mt-0.5 rounded
-  bg-slate-50 border border-gray-300;
+  bg-slate-50 border border-gray-300 dark:bg-slate-900
+  text-gray-700 dark:text-gray-300
+  transition-all duration-300;
 }
 </style>
