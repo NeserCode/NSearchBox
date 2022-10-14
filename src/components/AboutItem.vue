19d2315ec128d5eae7c1a0d168fe158f53261619
✨NeserCode <nesercode@gmail.com>
2022-10-14 17:01:38 +0800

    update setting options

---
+++ src/components/AboutItem.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { inject } from "vue"
+import { SettingItemKey } from "../tokens/settingItem"
+
+const { settingOption } = inject(SettingItemKey, undefined)
+</script>
+
+<template>
+	<div class="about-item-main"></div>
+</template>
+
+<style lang="postcss" scoped></style>
--- src/views/onSetting.vue
+++ src/views/onSetting.vue
@@ -1,48 +1,112 @@
 <script setup>
-import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue'
-import SettingItem from "../components/SettingItem.vue";
-import SearchOption from "../components/SearchOption.vue";
-import { searchOptions } from "../utils/searchOptions";
+import { TabGroup, TabList, Tab, TabPanels, TabPanel } from "@headlessui/vue"
+import SettingItem from "../components/SettingItem.vue"
+import SearchOption from "../components/SearchOption.vue"
+import { searchOptions } from "../utils/searchOptions"
 
-import { computed } from "vue";
+import { ref, computed } from "vue"
 const isSearchOptionsShow = computed(() => (option) => {
-  return option.type === "searchOption";
-});
+	return option.type === "searchOption"
+})
+
+const categories = ref({
+	searchOption: {
+		id: "1",
+		name: "搜索设置",
+	},
+	keyOption: {
+		id: "2",
+		name: "快捷键设置",
+	},
+	about: {
+		id: "3",
+		name: "关于",
+	},
+})
+
+const computedSelectedCategoryBtnClass = computed(() => (selected) => {
+	return selected ? "actived" : null
+})
 
 // const computedSearchOptionsJson = JSON.stringify(searchOptions);
 </script>
 
 <template>
-  <div class="setting-main">
-    <span>⚙ Testing the setting view.</span>
-    <TabGroup>
-    <TabList>
-      <Tab>Search Options</Tab>
-      <Tab>Tab 2</Tab>
-      <Tab>Tab 3</Tab>
-    </TabList>
-    <TabPanels>
-      <TabPanel>
-        <setting-item
-      v-for="option in searchOptions"
-      :key="option.id"
-      :settingOption="option"
-    >
-      <search-option v-if="isSearchOptionsShow(option)" />
-    </setting-item>
-      </TabPanel>
-      <TabPanel>Content 2</TabPanel>
-      <TabPanel>Content 3</TabPanel>
-    </TabPanels>
-  </TabGroup>
-  </div>
+	<div class="setting-main">
+		<TabGroup>
+			<TabList class="categoryList">
+				<Tab
+					v-for="category in categories"
+					as="template"
+					:key="category.id"
+					v-slot="{ selected }"
+				>
+					<button
+						:class="['categoryBtn', computedSelectedCategoryBtnClass(selected)]"
+					>
+						{{ category.name ?? category }}
+					</button>
+				</Tab>
+			</TabList>
+			<TabPanels class="categoryPanels">
+				<TabPanel class="categoryPanel">
+					<setting-item
+						v-for="option in searchOptions"
+						:key="option.id"
+						:settingOption="option"
+					>
+						<search-option v-if="isSearchOptionsShow(option)" />
+					</setting-item>
+				</TabPanel>
+				<TabPanel class="categoryPanel">Content 2</TabPanel>
+				<TabPanel class="categoryPanel">
+					<setting-item
+						v-for="option in searchOptions"
+						:key="option.id"
+						:settingOption="option"
+					>
+						<search-option v-if="isSearchOptionsShow(option)" />
+					</setting-item>
+				</TabPanel>
+			</TabPanels>
+		</TabGroup>
+	</div>
 </template>
 
 <style lang="postcss" scoped>
 .setting-main {
-  @apply w-full p-2 mt-0.5 rounded
-  bg-slate-50 border border-gray-300 dark:bg-slate-900
-  text-gray-700 dark:text-gray-300
+	@apply w-full p-2 mt-0.5 rounded px-8 pt-4
+  bg-slate-50 border border-gray-300 dark:bg-slate-900 text-gray-700 dark:text-gray-300
   transition-all duration-300;
+}
+
+.tab-container {
+	@apply w-full;
+}
+
+.categoryBtn {
+	@apply w-full rounded-lg py-2.5 text-sm font-medium leading-5
+  text-gray-700 dark:text-gray-300 hover:bg-white/[0.12] hover:text-blue-400
+  dark:hover:bg-slate-600 dark:hover:text-blue-400
+  outline-none;
+}
+.categoryBtn.actived {
+	@apply bg-white dark:bg-slate-800 shadow;
+}
+
+.categoryList {
+	@apply flex p-1 space-x-1 rounded-xl
+  bg-gray-100 dark:bg-gray-700 text-gray-700 dark:text-gray-300
+  outline-none;
+}
+
+.categoryPanels {
+	@apply my-2;
+}
+
+.categoryPanel {
+	@apply rounded-xl p-3
+  bg-white dark:bg-gray-700
+  outline-none;
 }
 </style>
