9af2127aa5c7bc8d5d3bba99ecec02645f5e067d
✨NeserCode <nesercode@gmail.com>
2022-10-27 16:24:57 +0800

    update icon file

--- vue.config.js
+++ vue.config.js
@@ -18,15 +18,15 @@ module.exports = defineConfig({
           "oneClick": false, // 是否一键安装
           "allowElevation": true, // 允许请求提升。 如果为false，则用户必须使用提升的权限重新启动安装程序。
           "allowToChangeInstallationDirectory": true, // 允许修改安装目录
-          "installerIcon": "./public/icon.ico", // 安装图标
-          "uninstallerIcon": "./public/icon.ico", //卸载图标
-          "installerHeaderIcon": "./public/icon.ico", // 安装时头部图标
+          "installerIcon": "./public/N.orange.ico", // 安装图标
+          "uninstallerIcon": "./public/N.orange.ico", //卸载图标
+          "installerHeaderIcon": "./public/N.orange.ico", // 安装时头部图标
           "createDesktopShortcut": true, // 创建桌面图标
           "createStartMenuShortcut": true, // 创建开始菜单图标
           "shortcutName": "NSearchBox", // 图标名称
         },
-        "win": { //win相关配置
-          "icon": "./public/icon.ico", //图标，当前图标在根目录下，注意这里有两个坑
+        "win": {
+          "icon": "./public/N.orange.ico",
           "target": [{
             "target": "nsis", //利用nsis制作安装程序
           }]
